645d35210937c21d2a27ddf73aae94f795d59fb0
Aladiev <Ilya.Aladev@leroymerlin.ru>
2025-06-02 01:24:09 +0300

    commit

--- test/unit/builders/dtos/createFilms/createFilmRequestDto.builder.ts
+++ test/unit/builders/dtos/createFilms/createFilmRequestDto.builder.ts
@@ -1,11 +1,16 @@
-import InjectionBuilder from "@/test/unit/builders/injectionBuilder";
+
+import BaseBuilder from "../../base.builder";
 import { CreateFilmRequestDto } from "../../../../../src/application/films/dtos/createFilm/createFilmRequest.dto";
+import { faker } from '@faker-js/faker';
 
 export class CreateFilmRequestDtoBuilder {
-  public static get defaultAll(): InjectionBuilder<CreateFilmRequestDto> {
-    return new InjectionBuilder<CreateFilmRequestDto>(new CreateFilmRequestDto()).with({
-      login: faker.login,
-      password: user.password,
+  public static get defaultAll(): BaseBuilder<CreateFilmRequestDto> {
+    return new BaseBuilder<CreateFilmRequestDto>(new CreateFilmRequestDto()).with({
+      title: faker,
+      year: faker,
+      duration,
+      description,
+      imageId,
     });
   }
 }
