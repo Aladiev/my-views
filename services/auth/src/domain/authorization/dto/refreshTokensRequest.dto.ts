077234c82bbc7042c51a0bac09661080d42399f6
aladiev.id <aladiev.id@barl.ru>
2025-07-19 18:20:56 +0300

    fixes

--- services/auth/src/domain/authorization/dto/refreshTokensRequest.dto.ts
+++ services/auth/src/domain/authorization/dto/refreshTokensRequest.dto.ts
@@ -1,13 +1,10 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsNumber, IsString } from "class-validator";
 
 export class RefreshTokensRequestDto {
   @ApiProperty({ example: 'saldasldsald' })
   refreshToken: string;
 
-  @IsString()
   email: string;
 
-  @IsNumber()
   expRef: number;
 };
